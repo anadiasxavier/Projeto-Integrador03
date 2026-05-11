552bea0caf71471b6d0a2be2cdff8d4c66552d01
Giovana <giibudri2228.com>
2026-05-11 20:45:58 -0300

    comentando arquivos

--- lib/models/player.dart
+++ lib/models/player.dart
@@ -1,8 +1,13 @@
-class Player {
+// Esse arquivo define a estrutura de dados do jogador.
+// Quais informações o jogo guarda sobre o personagem do jogador.
+//O class define quais atributos cada jogador terá. Podemos criar vários jogadores a partir dela
+class Player { 
 
   String nome;
   int nivel;
   int experiencia;
+
+  // O construtor é o que permite criar um jogador novo. O required significa que todos os três campos são obrigatórios — não dá pra criar um Player sem informar nome, nível e experiência.
 
   Player({
     required this.nome,
